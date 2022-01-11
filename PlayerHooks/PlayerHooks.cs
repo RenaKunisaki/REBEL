e1ec4c7847a959a335b55864c09f29781c5623cd
Rena Kunisaki <hyperhacker@gmail.com>
2022-01-11 18:18:08 -0500

    fuck

--- PlayerHooks/PlayerHooks.cs
+++ PlayerHooks/PlayerHooks.cs
@@ -8,9 +8,10 @@ using Microsoft.Xna.Framework;
 
 namespace REBEL.Hooks {
     public class PlayerHooks: ModPlayer {
-        public override void PreUpdate() {
+        protected void fuckingGravityShit1() {
             REBEL mod = Mod as REBEL;
             var player = Main.LocalPlayer;
+
             if(player.controlUp) {
                 //press Up to stop forcing inversion, in case
                 //you accidentally fall into space.
@@ -18,39 +19,20 @@ namespace REBEL.Hooks {
             }
 
             if(mod.forceUpsideDown) {
-                //var here = System.Reflection.MethodBase.GetCurrentMethod().Name;
-                //Main.NewText($"{here} grav={player.gravity} {player.gravDir}");    
-            }
-        }
-
-        public override void PreUpdateMovement() {
-            REBEL mod = Mod as REBEL;
-            var player = Main.LocalPlayer;
-            
-            if(mod.forceUpsideDown) {
-                //var here = System.Reflection.MethodBase.GetCurrentMethod().Name;
-                //Main.NewText($"{here} grav={player.gravity} {player.gravDir}");
-
-                //this has to be done here...
-                player.gravDir = -1f; //flips screen
-            }
-        }
-
-        public override void PostUpdateRunSpeeds() {
-            REBEL mod = Mod as REBEL;
-            var player = Main.LocalPlayer;
-            if(mod.forceUpsideDown) {
-                //var here = System.Reflection.MethodBase.GetCurrentMethod().Name;
-                //Main.NewText($"{here} grav={player.gravity} {player.gravDir}");
                 if(!mod.wasForceUpsideDown) {
                     Main.NewText("Going upside down", 0xFF, 0xFF, 0x00);
                     //Mod.Logger.Info("Going upside down");
                 }
                 mod.wasForceUpsideDown = true;
-
-                //...but this has to be done here.
-                if(player.gravity > 0) {
-                    player.gravity *= -1f; //flips gravity
+                player.gravDir = -1f; //flips screen
+            }
+            else if(mod.wasForceUpsideDown) {
+                mod.wasForceUpsideDown = false;
+                Main.NewText("Going upside up", 0xFF, 0xFF, 0x00);
+                //Mod.Logger.Info("Going upside up");
+                player.gravDir = 1f;
+                if(player.gravity < 0) {
+                    player.gravity *= -1f;
                 }
             }
 
@@ -59,25 +41,46 @@ namespace REBEL.Hooks {
             }
         }
 
-        public override void PostUpdate() {
+        protected void fuckingGravityShit2() {
             REBEL mod = Mod as REBEL;
             var player = Main.LocalPlayer;
             if(mod.forceUpsideDown) {
-                //var here = System.Reflection.MethodBase.GetCurrentMethod().Name;
-                //Main.NewText($"{here} grav={player.gravity} {player.gravDir}");
-            }
-
-            //undo gravity reversal if it's now turned off
-            if(mod.wasForceUpsideDown && !mod.forceUpsideDown) {
-                Main.NewText("Going upside up", 0xFF, 0xFF, 0x00);
-                //Mod.Logger.Info("Going upside up");
-                mod.wasForceUpsideDown = false;
-                player.gravDir = 1f;
-                if(player.gravity < 0) {
-                    player.gravity *= -1f;
+                 if(player.gravity > 0) {
+                    player.gravity *= -1f; //flips gravity
                 }
+                //player.gravControl = true; //does fuck all
             }
+        }
 
+        //I can have gravity inverted or be able to jump.
+        //not both. even though the game does it just fine.
+        //if I fuck with it just right I can also make it
+        //so you can only jump once.
+        public override void PreUpdate() {
+            //fuckingGravityShit1();
+            //fuckingGravityShit2();
+        }
+
+        public override void PreUpdateMovement() {
+            //fuckingGravityShit1();
+            fuckingGravityShit2();
+        }
+
+        public override void PostUpdateBuffs() {
+            //fuckingGravityShit1();
+            //fuckingGravityShit2();
+        }
+
+        public override void PostUpdateRunSpeeds() {
+            //fuckingGravityShit1();
+            //fuckingGravityShit2();
+        }
+
+        public override void PostUpdate() {
+            REBEL mod = Mod as REBEL;
+            var player = Main.LocalPlayer;
+            fuckingGravityShit1();
+            //fuckingGravityShit2();
             mod.checkTouchedBlocks(player);
         }
 
