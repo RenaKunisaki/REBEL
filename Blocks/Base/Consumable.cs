67473d4f15ff1ce49c41dba45111a3b2af9d1534
Rena Kunisaki <hyperhacker@gmail.com>
2022-01-19 15:15:14 -0500

    consumables: better actuation; drop the item when destroyed

--- Blocks/Base/Consumable.cs
+++ Blocks/Base/Consumable.cs
@@ -29,26 +29,46 @@ namespace REBEL.Blocks {
         public void OnTouched(Entity whom, Point location,
         TouchDirection direction) {
             var tile = Main.tile[location.X, location.Y];
-            if(tile.IsActuated) return; //don't react when turned off.
+            if(tile.HasActuator && tile.IsActuated) return; //don't react when turned off.
             if(tile.frameX != 0) return; //actuator hack
 
             if(whom is Player p) {
-                //WorldGen.KillTile(location.X, location.Y);
-                tile.IsActuated = true;
-                Point pt = getFrameBlock(location.X, location.Y);
-                setFrame(location.X, location.Y, 1, pt.Y);
-                Item.NewItem((int)whom.position.X, (int)whom.position.Y,
-                    16, 16, _getItemID());
+                if(tile.HasActuator) {
+                    //turn off (actuator hack)
+                    Point pt = getFrameBlock(location.X, location.Y);
+                    setFrame(location.X, location.Y, 1, pt.Y);
+                    //spawn the item right on the player
+                    Item.NewItem((int)whom.position.X, (int)whom.position.Y,
+                        16, 16, _getItemID());
+                }
+                //don't drop an item if we're about to destroy the block,
+                //because doing so will drop the item anyway.
+                else WorldGen.KillTile(location.X, location.Y);
             }
         }
 
         public override void HitWire(int i, int j) {
             Tile tile = Main.tile[i, j];
+            Point p = getFrameBlock(i, j);
             if(tile.HasActuator) {
                 //actuation doesn't work properly for non-solid blocks.
-                Point p = getFrameBlock(i, j);
-                setFrame(i, j, p.X ^ 1, p.Y);
+                //anyway, we want actuators to only enable, not disable,
+                //so we can use them to reset a bunch without worrying
+                //about ones that weren't collected.
+                setFrame(i, j, 0, p.Y);
             }
+        }
+
+        public override void KillTile(int i, int j, ref bool fail,
+        ref bool effectOnly, ref bool noItem) {
+            /** Called when this tile is hit by a pickaxe.
+             *  i, j: this tile's world tile-coordinates.
+             *  fail: whether we didn't hit hard enough to destroy it.
+             *  effectOnly: only create dust
+             *  noItem: don't drop anything.
+             */
+            if(fail || effectOnly || noItem) return;
+            Item.NewItem(i * 16, j * 16, 16, 16, _getItemID());
         }
 
         //repeat the middle frame to avoid duplicating it in memory.
